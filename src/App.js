4a4db207445c12ae5d4bc45a753d8e14743bcd8c
unknown <narinzeanthony@yahoo.com>
2021-10-07 06:30:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages';
 import signinPage from './components/pages/signin';
-import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={signinPage}  />
